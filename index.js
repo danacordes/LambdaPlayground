f7e32320d4ef5f1a4bb3e1ec23cbd5dc90378140
EC2 Default User <ec2-user@ip-172-31-13-77.us-west-2.compute.internal>
2018-12-06 05:55:54 +0000

    clearing static content adding AWS library, and region config

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 'use strict';
 
+var AWS = require('aws-sdk');
 var fs = require('fs');
 var path = require('path');
+
+var REGION = 'us-west-2';
+AWS.config.update({region: REGION});
 
 exports.get = function(event, context, callback) {
   var contents = fs.readFileSync(`public${path.sep}index.html`);
