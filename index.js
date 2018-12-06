5c2c282383f0ff6163c06b65064927bdb4a7fc5d
danacordes <ec2-user@ip-172-31-13-77.us-west-2.compute.internal>
2018-12-06 06:20:22 +0000

    Ensuring test will pass

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 'use strict';
 
-var AWS = require('aws-sdk');
 var fs = require('fs');
 var path = require('path');
+var AWS = require('aws-sdk');
 
 var REGION = 'us-west-2';
 AWS.config.update({region: REGION});
+
+var ddb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
+
 
 exports.get = function(event, context, callback) {
   var contents = fs.readFileSync(`public${path.sep}index.html`);
